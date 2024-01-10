cb9ba6599b21fe582cfe1635bd380cd406c7fec9
Bintang <bintangadiputrapratama@gmail.com>
2024-01-10 16:04:48 +0700

    [nvim] Y whtever

--- .config/nvchad/plugins.lua
+++ .config/nvchad/plugins.lua
@@ -51,25 +51,6 @@ local M = {
 
   {
     "lewis6991/gitsigns.nvim",
-    init = function()
-      vim.api.nvim_create_autocmd({ "BufRead" }, {
-        group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
-        callback = function()
-          vim.fn.jobstart({"git", "-C", vim.loop.cwd(), "rev-parse"},
-            {
-              on_exit = function(_, return_code)
-                if return_code == 0 then
-                  vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
-                  vim.schedule(function()
-                    require("lazy").load { plugins = { "gitsigns.nvim" } }
-                  end)
-                end
-              end
-            }
-          )
-        end,
-      })
-    end,
     opts = function()
       return vim.tbl_deep_extend("force", require("plugins.configs.others").gitsigns, require("custom.configs.others").gitsigns)
     end,
